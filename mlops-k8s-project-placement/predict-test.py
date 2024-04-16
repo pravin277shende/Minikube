719ed3b558be3c9486e1d24bfff817b62829f495
pravin277shende <167225629+pravin277shende@users.noreply.github.com>
2024-04-16 23:17:12 +0530

    Add files via upload
    Initial commit

---
+++ mlops-k8s-project-placement/predict-test.py
@@ -0,0 +1,23 @@
+import requests
+
+candidate = [{"gender": "M",
+  "ssc_p": 31.0,
+  "ssc_b": 'Central',
+  "hsc_p": 38.66,
+  "hsc_b": 'Central',
+  "hsc_s": 'Science',
+  "degree_p": 38.0,
+  "degree_t": 'Sci&Tech',
+  "etest_p": 26.0,
+  "mba_p": 21.3,
+  "specialisation": 'Mkt&Fin',
+  "workex": 'Yes',
+  }]
+url = "http://localhost:9696/predict"
+response = requests.post(url=url, json=candidate)
+
+if response.status_code == 200:
+    output = response.json()
+    print(f'Candidate features have been evaluated and output is {output}')
+else:
+    print(f'Error in evaluating features, status code: {response.status_code}')
